35bbfb576037ecff6a8ed19dda043af93f6ffc56
anjamodrinic <am20190184@student.fon.bg.ac.rs>
2022-12-28 17:48:54 +0100

    Modifikovani prethodno napravljeni modeli

--- skiLaravel/database/migrations/2022_12_28_154004_create_skis_table.php
+++ skiLaravel/database/migrations/2022_12_28_154004_create_skis_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('skis', function (Blueprint $table) {
             $table->id();
+            $table->string('model');
+            $table->string('color');
+            $table->int('length');
             $table->timestamps();
         });
     }
